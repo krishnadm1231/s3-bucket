db854682cb2523e2595223c3b083d9935445eead
Krishnamurthy D M <133624906+krishnadm1231@users.noreply.github.com>
2023-11-18 15:12:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,11 @@ provider "aws" {
 
 resource "aws_s3_bucket" "Terraform-s3" {
   bucket = var.bucket
-  force_destroy = var.force_destroy
 }
 
 resource "aws_s3_bucket_versioning" "Terraform-s3-versioning" {
   bucket = aws_s3_bucket.Terraform-s3.id
   versioning_configuration {
-    status = var.versioning
+    status = Enabled
   }
 }
